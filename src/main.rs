5c284c4eefd42707c035d8bef02050f13514f95f
Oliver Gould <ver@buoyant.io>
2021-04-05 03:31:23 +0000

    Sketch out grpc api

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![allow(warnings)]
 
 use futures::{future, prelude::*};
-use k8s_openapi::api::core::v1::Pod;
 use kube::{
     api::{DynamicObject, ListParams, Resource},
     Api,
@@ -20,90 +19,25 @@ type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let (drain_tx, drain_rx) = linkerd_drain::channel();
+
     let client = kube::Client::try_default()
         .await
         .expect("Failed to initialize client");
-
-    // let authz_api = Api::<authz::Authorization>::all(client.clone());
-    // let authz_task = tokio::spawn(
-    //     async move {
-    //         let mut authz = watcher(authz_api, ListParams::default()).boxed();
-    //         while let some(ev) = authz.try_next().await? {
-    //             match ev {
-    //                 event::applied(authz) => {
-    //                     info!(
-    //                         ns = %authz.namespace().unwrap_or_default(),
-    //                         name = %authz.name(),
-    //                         "applied",
-    //                     );
-    //                     debug!("{:#?}", authz);
-    //                 }
-    //                 event::deleted(authz) => {
-    //                     info!(
-    //                         ns = %authz.namespace().unwrap_or_default(),
-    //                         name = %authz.name(),
-    //                         "deleted",
-    //                     );
-    //                     debug!("{:#?}", authz);
-    //                 }
-    //                 event::restarted(authzs) => {
-    //                     info!("restarted");
-    //                     for authz in authzs.into_iter() {
-    //                         info!(
-    //                             ns = %authz.namespace().unwrap_or_default(),
-    //                             name = %authz.name(),
-    //                         );
-    //                         debug!("{:#?}", authz);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         ok::<(), kube_runtime::watcher::error>(())
-    //     }
-    //     .instrument(info_span!("authz")),
-    // );
-
-    //let srv_api = Api::<server::Server>::all(client);
-    // let srv_task = tokio::spawn(
-    //     async move {
-    //         let mut srvs = watcher(srv_api, ListParams::default()).boxed();
-    //         while let Some(ev) = srvs.try_next().await? {
-    //             match ev {
-    //                 Event::Applied(srv) => {
-    //                     info!(ns = %srv.namespace().unwrap(), name = %srv.name(), "Applied");
-    //                     debug!("{:#?}", srv);
-    //                 }
-    //                 Event::Deleted(srv) => {
-    //                     info!(ns = %srv.namespace().unwrap(), name = %srv.name(), "Deleted");
-    //                     debug!("{:#?}", srv);
-    //                 }
-    //                 Event::Restarted(srvs) => {
-    //                     info!("Restarted");
-    //                     for srv in srvs.into_iter() {
-    //                         info!(ns = %srv.namespace().unwrap(), name = %srv.name());
-    //                         debug!("{:#?}", srv);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         Ok::<(), kube_runtime::watcher::Error>(())
-    //     }
-    //     .instrument(info_span!("srv")),
-    // );
-
-    let (drain_tx, drain_rx) = linkerd_drain::channel();
+    let (watcher, indexer) = state::spawn(client);
 
     let addr = ([0, 0, 0, 0], 8090).into();
+    let server = grpc::Grpc::new(watcher, drain_rx.clone());
     let grpc = tokio::spawn(
         async move {
-            let (tx, rx) = tokio::sync::oneshot::channel();
+            let (close_tx, close_rx) = tokio::sync::oneshot::channel();
             tokio::pin! {
-                let srv = grpc::serve(addr, rx.map(|_| {}));
+                let srv = server.serve(addr, close_rx.map(|_| {}));
             }
             let res = tokio::select! {
                 res = (&mut srv) => res,
                 handle = drain_rx.signaled() => {
-                    let _ = tx.send(());
+                    let _ = close_tx.send(());
                     handle.release_after(srv).await
                 }
             };
@@ -126,62 +60,162 @@ async fn main() {
 
     tokio::join!(ctrl_c, term);
     drain_tx.drain().await;
-    grpc.await.expect("Spawn must succeed")
+    grpc.await.expect("Spawn must succeed");
+    indexer.await.expect("Spawn must succeed");
 }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct WatchKey {
-    ns: Option<String>,
-    name: String,
-}
+mod state {
+    use super::{authz::Authorization, grpc::proto, server::Server};
+    use k8s_openapi::api::core::v1::Pod;
+    use std::{
+        collections::{HashMap, HashSet},
+        pin::Pin,
+        sync::{Arc, Weak},
+    };
+    use tokio::sync::{mpsc, watch, Mutex, Notify};
 
-#[derive(Debug)]
-struct WatchUpdater<T> {
-    rx: watch::Receiver<T>,
-    tx: watch::Sender<T>,
-}
+    #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+    struct AuthzKey {
+        ns: String,
+        name: String,
+    }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct AuthzKey {
-    ns: String,
-    name: String,
-}
+    #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+    struct SrvKey {
+        ns: String,
+        name: String,
+    }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct SrvKey {
-    ns: String,
-    name: String,
-}
+    #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+    struct PodKey {
+        ns: String,
+        name: String,
+    }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct PodKey {
-    ns: String,
-    name: String,
-}
+    #[derive(Clone, Debug, Hash, PartialEq, Eq)]
+    struct PortKey {
+        pod: PodKey,
+        port: u16,
+    }
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq)]
-struct PortKey {
-    pod: PodKey,
-    port: u16,
-}
+    #[derive(Debug)]
+    struct ServerState {
+        authorizations: HashSet<AuthzKey>,
+        pods: HashSet<PodKey>,
+        tx: watch::Receiver<proto::InboundProxyConfig>,
+        rx: watch::Sender<proto::InboundProxyConfig>,
+    }
 
-struct ServerState {
-    authorizations: HashSet<AuthzKey>,
-    pods: HashSet<PodKey>,
-    tx: watch::Sender<grpc::proto::InboundProxyConfig>,
-    rx: watch::Sender<grpc::proto::InboundProxyConfig>,
-}
+    #[derive(Debug, Default)]
+    pub struct State {
+        authorizations: HashMap<AuthzKey, Arc<Authorization>>,
+        servers: HashMap<SrvKey, ServerState>,
+        pods: HashMap<PodKey, HashMap<u16, Option<SrvKey>>>,
+        lookups: HashMap<PortKey, Vec<Weak<Notify>>>,
+    }
 
-struct State {
-    authorizations: HashMap<AuthzKey, Arc<authz::Authorization>>,
-    servers: HashMap<SrvKey, ServerState>,
-    pods: HashMap<PodKey, HashMap<u16, SrvKey>>,
-    lookups: HashMap<PortKey, Vec<Weak<Notify>>>,
+    #[derive(Clone, Debug)]
+    pub struct Watcher(Arc<Mutex<State>>);
+
+    pub type Watch = watch::Receiver<proto::InboundProxyConfig>;
+
+    impl Watcher {
+        pub async fn watch(
+            &self,
+            ns: impl Into<String>,
+            name: impl Into<String>,
+            port: u16,
+        ) -> Watch {
+            let ns = ns.into();
+            let name = name.into();
+            todo!("watch stream")
+        }
+    }
+
+    pub fn spawn(client: kube::Client) -> (Watcher, tokio::task::JoinHandle<()>) {
+        let state = Arc::new(Mutex::new(State::default()));
+        let watcher = Watcher(state.clone());
+        let task = tokio::spawn(index(client, state));
+        (watcher, task)
+    }
+
+    async fn index(client: kube::Client, state: Arc<Mutex<State>>) {
+        // let authz_api = Api::<authz::Authorization>::all(client.clone());
+        // let srv_api = Api::<server::Server>::all(client);
+        // let pod_api = Api::<Pod>::all(client);
+
+        // let authz_task = tokio::spawn(
+        //     async move {
+        //         let mut authz = watcher(authz_api, ListParams::default()).boxed();
+        //         while let some(ev) = authz.try_next().await? {
+        //             match ev {
+        //                 event::applied(authz) => {
+        //                     info!(
+        //                         ns = %authz.namespace().unwrap_or_default(),
+        //                         name = %authz.name(),
+        //                         "applied",
+        //                     );
+        //                     debug!("{:#?}", authz);
+        //                 }
+        //                 event::deleted(authz) => {
+        //                     info!(
+        //                         ns = %authz.namespace().unwrap_or_default(),
+        //                         name = %authz.name(),
+        //                         "deleted",
+        //                     );
+        //                     debug!("{:#?}", authz);
+        //                 }
+        //                 event::restarted(authzs) => {
+        //                     info!("restarted");
+        //                     for authz in authzs.into_iter() {
+        //                         info!(
+        //                             ns = %authz.namespace().unwrap_or_default(),
+        //                             name = %authz.name(),
+        //                         );
+        //                         debug!("{:#?}", authz);
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //         ok::<(), kube_runtime::watcher::error>(())
+        //     }
+        //     .instrument(info_span!("authz")),
+        // );
+
+        // let srv_task = tokio::spawn(
+        //     async move {
+        //         let mut srvs = watcher(srv_api, ListParams::default()).boxed();
+        //         while let Some(ev) = srvs.try_next().await? {
+        //             match ev {
+        //                 Event::Applied(srv) => {
+        //                     info!(ns = %srv.namespace().unwrap(), name = %srv.name(), "Applied");
+        //                     debug!("{:#?}", srv);
+        //                 }
+        //                 Event::Deleted(srv) => {
+        //                     info!(ns = %srv.namespace().unwrap(), name = %srv.name(), "Deleted");
+        //                     debug!("{:#?}", srv);
+        //                 }
+        //                 Event::Restarted(srvs) => {
+        //                     info!("Restarted");
+        //                     for srv in srvs.into_iter() {
+        //                         info!(ns = %srv.namespace().unwrap(), name = %srv.name());
+        //                         debug!("{:#?}", srv);
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //         Ok::<(), kube_runtime::watcher::Error>(())
+        //     }
+        //     .instrument(info_span!("srv")),
+        // );
+
+        todo!("indexing task")
+    }
 }
 
 mod grpc {
-    use futures::Stream;
-    use std::pin::Pin;
+    use super::state::{Watch, Watcher};
+    use futures::prelude::*;
 
     pub mod proto {
         tonic::include_proto!("polixy.olix0r.net");
@@ -191,27 +225,32 @@ mod grpc {
         };
     }
 
-    pub struct Grpc;
-
-    pub async fn serve(
-        addr: std::net::SocketAddr,
-        shutdown: impl std::future::Future<Output = ()>,
-    ) -> Result<(), tonic::transport::Error> {
-        tonic::transport::Server::builder()
-            .add_service(Grpc::server())
-            .serve_with_shutdown(addr.into(), shutdown)
-            .await
+    #[derive(Clone, Debug)]
+    pub struct Grpc {
+        watcher: Watcher,
+        drain: linkerd_drain::Watch,
     }
 
     impl Grpc {
-        pub fn server() -> proto::Server<Self> {
-            proto::Server::new(Self)
+        pub fn new(watcher: Watcher, drain: linkerd_drain::Watch) -> Self {
+            Self { watcher, drain }
+        }
+
+        pub async fn serve(
+            self,
+            addr: std::net::SocketAddr,
+            shutdown: impl std::future::Future<Output = ()>,
+        ) -> Result<(), tonic::transport::Error> {
+            tonic::transport::Server::builder()
+                .add_service(proto::Server::new(self))
+                .serve_with_shutdown(addr.into(), shutdown)
+                .await
         }
     }
 
     #[async_trait::async_trait]
     impl proto::Service for Grpc {
-        type WatchInboundStream = Pin<
+        type WatchInboundStream = std::pin::Pin<
             Box<dyn Stream<Item = Result<proto::InboundProxyConfig, tonic::Status>> + Send + Sync>,
         >;
 
@@ -219,7 +258,42 @@ mod grpc {
             &self,
             req: tonic::Request<proto::InboundProxyPort>,
         ) -> Result<tonic::Response<Self::WatchInboundStream>, tonic::Status> {
-            todo!("Lookup server on pod ")
+            let proto::InboundProxyPort { workload, port } = req.into_inner();
+            // parse a workload name in the form namespace:name.
+
+            let (ns, name) = {
+                let parts = workload.splitn(2, ':').collect::<Vec<_>>();
+                if parts.len() != 2 {
+                    return Err(tonic::Status::invalid_argument(format!(
+                        "Invalid workload: {}",
+                        workload
+                    )));
+                }
+                (parts[0], parts[1])
+            };
+
+            if port > std::u16::MAX as u32 {
+                return Err(tonic::Status::invalid_argument(format!(
+                    "Invalid port: {}",
+                    port
+                )));
+            }
+
+            let mut watch = self.watcher.watch(ns, name, port as u16).await;
+            Ok(tonic::Response::new(Box::pin(async_stream::try_stream! {
+                loop {
+                    let update = (*watch.borrow()).clone();
+                    yield update;
+
+                    tokio::select! {
+                        res = watch.changed() => {
+                            if res.is_err() {
+                                return;
+                            }
+                        }
+                    }
+                }
+            })))
         }
     }
 }

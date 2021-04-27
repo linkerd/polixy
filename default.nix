40c9484ae6a2c26235f9a88dc26ef8746baa7d19
Oliver Gould <ver@buoyant.io>
2021-04-27 05:39:14 +0000

    Add nix configuration

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+
+buildEnv {
+  name = "linkerd2-proxy-env";
+  paths =  [
+      binutils
+      cacert
+      clang
+      docker
+      git
+      (glibcLocales.override { locales = [ "en_US.UTF-8" ]; })
+      cmake
+      openssl
+      pkg-config
+      protobuf
+      rustup
+      stdenv
+    ];
+
+  passthru = with pkgs; {
+    CARGO_TERM_COLOR = "always";
+    CURL_CA_BUNDLE = "${cacert}/etc/ca-bundle.crt";
+    GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+    LC_ALL = "en_US.UTF-8";
+    OPENSSL_DIR = "${openssl.dev}";
+    OPENSSL_LIB_DIR = "${openssl.out}/lib";
+    PROTOC = "${protobuf}/bin/protoc";
+    PROTOC_INCLUDE = "${protobuf}/include";
+    RUST_BACKTRACE = "full";
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+
+let env = (import ./default.nix scope);
+
+in mkShell { 
+  CARGO_TERM_COLOR = "always";
+  CURL_CA_BUNDLE = "${cacert}/etc/ca-bundle.crt";
+  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  LC_ALL = "en_US.UTF-8";
+  LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+  OPENSSL_DIR = "${openssl.dev}";
+  OPENSSL_LIB_DIR = "${openssl.out}/lib";
+  PROTOC = "${protobuf}/bin/protoc";
+  PROTOC_INCLUDE = "${protobuf}/include";
+  RUST_BACKTRACE = "1";
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+  buildInputs = [ (import ./default.nix { inherit pkgs; }) ];
+}

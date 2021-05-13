43bbfa7674b2f76f06069c56155ff5792a5cece2
Oliver Gould <ver@buoyant.io>
2021-05-13 06:15:11 +0000

    Extract node ip indexing into a seperate file

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -1,3 +1,5 @@
+mod node;
+
 use crate::{
     k8s::{self, polixy},
     ClientAuthn, ClientAuthz, ClientNetwork, DefaultMode, FromResource, Identity,
@@ -325,98 +327,6 @@ impl Index {
                 warn!(?error);
             }
         }
-    }
-
-    // === Node->IP indexing ===
-
-    #[instrument(
-        skip(self, node),
-        fields(name = ?node.metadata.name)
-    )]
-    fn apply_node(&mut self, node: k8s::Node) -> Result<()> {
-        let name = k8s::NodeName::from_resource(&node);
-
-        match self.node_ips.entry(name) {
-            HashEntry::Vacant(entry) => {
-                let ips = Self::kubelet_ips(node)
-                    .with_context(|| format!("failed to load kubelet IPs for {}", entry.key()))?;
-                debug!(?ips, "Adding");
-                entry.insert(ips);
-            }
-            HashEntry::Occupied(_) => trace!("Already existed"),
-        }
-
-        Ok(())
-    }
-
-    #[instrument(
-        skip(self, node),
-        fields(name = ?node.metadata.name)
-    )]
-    fn delete_node(&mut self, node: k8s::Node) -> Result<()> {
-        let name = k8s::NodeName::from_resource(&node);
-        if self.node_ips.remove(&name).is_some() {
-            debug!("Deleted");
-            Ok(())
-        } else {
-            Err(anyhow!("node {} already deleted", name))
-        }
-    }
-
-    #[instrument(skip(self, nodes))]
-    fn reset_nodes(&mut self, nodes: Vec<k8s::Node>) -> Result<()> {
-        // Avoid rebuilding data for nodes that have not changed.
-        let mut prior_names = self.node_ips.keys().cloned().collect::<HashSet<_>>();
-
-        let mut result = Ok(());
-        for node in nodes.into_iter() {
-            let name = k8s::NodeName::from_resource(&node);
-            if !prior_names.remove(&name) {
-                if let Err(error) = self.apply_node(node) {
-                    warn!(%name, %error, "Failed to apply node");
-                    result = Err(error);
-                }
-            } else {
-                trace!(%name, "Already existed");
-            }
-        }
-
-        for name in prior_names.into_iter() {
-            debug!(?name, "Removing defunct node");
-            let removed = self.node_ips.remove(&name).is_some();
-            debug_assert!(removed, "node must be removable");
-            if !removed {
-                result = Err(anyhow!("node {} already removed", name));
-            }
-        }
-
-        result
-    }
-
-    fn kubelet_ips(node: k8s::Node) -> Result<KubeletIps> {
-        let spec = node.spec.ok_or_else(|| anyhow!("node missing spec"))?;
-
-        let addrs = if let Some(nets) = spec.pod_cidrs {
-            nets.into_iter()
-                .map(Self::cidr_to_kubelet_ip)
-                .collect::<Result<Vec<_>>>()?
-        } else {
-            let cidr = spec
-                .pod_cidr
-                .ok_or_else(|| anyhow!("node missing pod_cidr"))?;
-            let ip = Self::cidr_to_kubelet_ip(cidr)?;
-            vec![ip]
-        };
-
-        Ok(KubeletIps(addrs.into()))
-    }
-
-    fn cidr_to_kubelet_ip(cidr: String) -> Result<IpAddr> {
-        cidr.parse::<IpNet>()
-            .with_context(|| format!("invalid CIDR {}", cidr))?
-            .hosts()
-            .next()
-            .ok_or_else(|| anyhow!("pod CIDR network is empty"))
     }
 
     // === Namespace indexing ===
